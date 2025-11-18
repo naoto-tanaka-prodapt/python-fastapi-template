7f1fc23fcf09de25ea03e1dcb47665e257f8bf1b
naoto.tanaka <OI.naoto@prodapt.com>
2025-11-18 17:41:21 +0530

    add: job-board path

--- main.py
+++ main.py
@@ -1,6 +1,39 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 
 app = FastAPI()
+
+jobs_data = {
+    "acme": [
+      {
+        "title": "Customer Support Executive",
+        "descrition": "Customer Support"
+      },
+      {
+        "title": "Project Manager",
+        "descrition": "Project Management"
+      }
+    ],
+    "bcg": [
+      {
+        "title": "Technical Architect",
+        "descrition": "Architect"
+      },
+      {
+        "title": "Junior Sowtware Engineer",
+        "descrition": "Junior Sowtware Engineer"
+      }
+    ],
+    "atlas": [
+      {
+        "title": "Technical Architect",
+        "descrition": "Architect"
+      },
+      {
+        "title": "Junior Sowtware Engineer",
+        "descrition": "Junior Sowtware Engineer"
+      }
+    ]
+  }
 
 @app.get("/")
 async def index():
@@ -23,3 +56,12 @@ async def multiply(x: int, y: int):
   return {
     "result": result
   }
+
+@app.get("/job-board/{company_name}")
+async def get_jobs(company_name: str):
+  try: 
+    jobs = jobs_data[company_name]
+  except:
+    raise HTTPException(status_code=404, detail="Company not found")
+  
+  return jobs
